23cf6c55b7605816b21bfb5d1f9d3c16ca708c3e
SwatiRao07 <107425238+SwatiRao07@users.noreply.github.com>
2022-11-26 10:23:22 +0530

    Add files via upload

---
+++ table.php
@@ -0,0 +1,32 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "file";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+// sql to create table
+$sql = "CREATE TABLE contact_details (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+COMPANY_NAME VARCHAR(20) NOT NULL,
+FIRST_NAME VARCHAR(15) NOT NULL,
+LAST_NAME VARCHAR(15) NOT NULL,
+PHONE int(10) NOT NULL,
+EMAIL varchar(30) NOT NULL,
+COMMENTS varchar(100) 
+)";
+
+if ($conn->query($sql) === TRUE) {
+  echo "Table Registration created successfully";
+} else {
+  echo "Error creating table: " . $conn->error;
+}
+
+$conn->close();
+?>
